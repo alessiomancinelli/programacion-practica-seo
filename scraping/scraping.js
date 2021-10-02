021bbee3f7170d33cfc9e1ed3d7a602336acaaf8
unknown <mancinelli.alessio@gmail.com>
2021-10-02 10:14:40 +0100

    Update Scraping

--- scraping/scraping.js
+++ scraping/scraping.js
@@ -1,6 +1,20 @@
+const axios = require['axios'];
+const jsdom = require['jsdom'];
 
 
-//Request
+
+//Request - ora fare richiesta
+//const response = axios.get('https://seohacks.es');
+//console.log(response);
+
+
+async function fetch(url) {
+    const response = await axios.get(url);
+    console.log(response);
+
+}
+
+fetch('https://seohacks.es');
 
 
 //Parsing
